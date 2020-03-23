214f430b226ff8ecad3dbc92bf29638a5c209145
James Truitt <>
2020-03-23 15:49:52 -0400

    update

--- scripts/installMediaCentral.bash
+++ scripts/installMediaCentral.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo Password12345 | sudo sh -c 'echo root:Password12345' | chpasswd
-echo Password12345 | su
+echo 'Password12345' | sudo sh -c 'echo root:Password12345' | chpasswd
+echo 'Password12345' | su
